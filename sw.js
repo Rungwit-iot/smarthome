e3cb6bccc78a9f51be720b99e378756db27e8acc
Rungwit-iot <81887621+Rungwit-iot@users.noreply.github.com>
2026-07-23 23:23:00 +0700

    Cache new quiz and simulated plant database

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='plant-guardian-v4';
-const ASSETS=['./','./index.html','./plants.json','./firebase-config.js','./manifest.webmanifest'];
+const CACHE='plant-guardian-v5';
+const ASSETS=['./','./index.html','./quiz.html','./plants.json','./plants-legacy.json','./plants-v2.js','./firebase-config.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',event=>{
