876fa41a813321b70be9986b0e95b12101c84dea
Rungwit-iot <81887621+Rungwit-iot@users.noreply.github.com>
2026-07-23 20:36:22 +0700

    Add Firebase configuration template

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "ใส่_API_KEY_ที่นี่",
+  authDomain: "ใส่_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://ใส่_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ใส่_PROJECT_ID",
+  storageBucket: "ใส่_PROJECT_ID.appspot.com",
+  messagingSenderId: "ใส่_SENDER_ID",
+  appId: "ใส่_APP_ID"
+};
